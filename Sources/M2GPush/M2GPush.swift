2f1a54c4af2c93b3e5fc60d47fc00cfcf2f4dfff
platib <platib85@gmail.com>
2024-05-14 16:49:00 +0900

    test code

--- Sources/M2GPush/M2GPush.swift
+++ Sources/M2GPush/M2GPush.swift
@@ -1,2 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct M2GPush {
+    public private(set) var text = "Hello, World!"
+    
+    public init() {
+        
+    }
+    
+    public func test() {
+        print("Test Completed!")
+    }
+}
