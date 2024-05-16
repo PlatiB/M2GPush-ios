39ea23c9073001d3c0eb5989e84f66e1e3a3b6ed
platib <platib85@gmail.com>
2024-05-16 12:55:17 +0900

    add firebase

--- Package.swift
+++ Package.swift
@@ -11,11 +11,21 @@ let package = Package(
             name: "M2GPush",
             targets: ["M2GPush"]),
     ],
+    dependencies: [
+        .package(
+            name: "Firebase",
+             url: "https://github.com/firebase/firebase-ios-sdk",
+            .upToNextMajor(from: "10.26.0")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "M2GPush"),
+            name: "M2GPush",
+            dependencies: [
+                .product(name: "FirebaseMessaging", package: "Firebase")
+            ]
+        ),
         .testTarget(
             name: "M2GPushTests",
             dependencies: ["M2GPush"]),
--- Sources/M2GPush/M2GPush.swift
+++ Sources/M2GPush/M2GPush.swift
@@ -1,14 +1,59 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
+import UIKit
 
-public struct M2GPush {
+public class M2GPush: NSObject, UNUserNotificationCenterDelegate, MessagingDelegate {
     public private(set) var text = "Hello, World!"
-    
-    public init() {
-        
+
+    public override init() {
+        super.init()
+        print("init M2G push")
+        configureFirebase()
     }
-    
-    public func test() {
-        print("Test Completed!")
+
+    private func configureFirebase() {
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+    }
+
+    public func registerForNotifications() {
+        UNUserNotificationCenter.current().delegate = self
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { granted, error in
+            if let error = error {
+                print("Failed to request authorization for notifications: \(error.localizedDescription)")
+                return
+            }
+            if granted {
+                print("Notification authorization granted.")
+            } else {
+                print("Notification authorization denied.")
+            }
+        }
+        UIApplication.shared.registerForRemoteNotifications()
+    }
+
+    public func registerToken() {
+        print("register token to m2g")
+    }
+
+    // Handle received FCM token
+    public func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        if let token = fcmToken {
+            print("FCM Token: \(token)")
+            // Send the FCM token to your server if needed
+        } else {
+            print("Failed to retrieve FCM token")
+        }
+    }
+
+    // Show notifications while the app is in the foreground
+    public func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        if #available(iOS 14.0, *) {
+            completionHandler([.banner, .sound, .badge])
+        } else {
+            completionHandler([.alert, .sound, .badge])
+        }
     }
 }
